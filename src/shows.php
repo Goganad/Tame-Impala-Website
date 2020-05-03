05788b4463a69ef1e949cb1096606c5b4a9e071c
Igor Nadenenko <55407158+Goganad@users.noreply.github.com>
2020-05-03 23:18:36 +0300

    PHP support
    Changed source files extensions to ".php". Changed index page logic

---
+++ src/shows.php
@@ -0,0 +1,89 @@
+
+<div class="tour">
+    <h1>SHOWS</h1>
+    <div class="show">
+        <div id = "show-info">
+            <h3>Monday, March 9, 2020</h3>
+            <p>8:00 pm</p>
+            <p>San Diego, CA</p>
+            <p>Penchanga Arena San Diego</p>
+            <p>with special guest Clairo</p>
+        </div>
+        <div id = "buy-tickets">
+            <div id = "buy-tickets-button">
+                <a href="#">Buy Tickets</a>
+            </div>
+        </div>
+    </div>
+    <div class="show">
+        <div id = "show-info">
+            <h3>Tuesday, March 10, 2020</h3>
+            <p>8:00 pm</p>
+            <p>Inglewood, CA</p>
+            <p>The Forum</p>
+            <p>with special guest Clairo</p>
+        </div>
+        <div id = "buy-tickets">
+            <div id = "buy-tickets-button">
+                <a href="#">Buy Tickets</a>
+            </div>
+        </div>
+    </div>
+    <div class="show">
+        <div id = "show-info">
+            <h3>Friday, March 13, 2020</h3>
+            <p>7:30 pm</p>
+            <p>San Francisco, CA</p>
+            <p>Chase Center</p>
+            <p>with special guest Clairo</p>
+        </div>
+        <div id = "buy-tickets">
+            <div id = "buy-tickets-button">
+                <a href="#">Buy Tickets</a>
+            </div>
+        </div>
+    </div>
+    <div class="show">
+        <div id = "show-info">
+            <h3>Thursday, March 19, 2020</h3>
+            <p>7:00 pm</p>
+            <p>Mexico City, MX</p>
+            <p>Foro Sol</p>
+            <p>with special guest MGMT and Clairo</p>
+        </div>
+        <div id = "buy-tickets">
+            <div id = "buy-tickets-button">
+                <a href="#">Buy Tickets</a>
+            </div>
+        </div>
+    </div>
+    <div class="show">
+        <div id = "show-info">
+            <h3>Thursday, April 16, 2020</h3>
+            <p>19:00</p>
+            <p>Auckland, New Zealand</p>
+            <p>Spark Arena</p>
+            <p>with special guest Khruangbin</p>
+        </div>
+        <div id = "buy-tickets">
+            <div id = "buy-tickets-button">
+                <a href="#">Buy Tickets</a>
+            </div>
+        </div>
+    </div>
+    <div class="show">
+        <div id = "show-info">
+            <h3>Saturday, April 18, 2020</h3>
+            <p>20:00 pm</p>
+            <p>Brisbane, Australia</p>
+            <p>Brisbane Entertainment Center</p>
+            <p>with special guest Khruangbin</p>
+        </div>
+        <div id = "buy-tickets">
+            <div id = "buy-tickets-button">
+                <a href="#">Buy Tickets</a>
+            </div>
+        </div>
+    </div>
+</div>
+    
